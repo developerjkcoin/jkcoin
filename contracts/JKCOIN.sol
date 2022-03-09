3fc31a8f2e3ed4b398e18acffc4eeb21e0430a6d
supakornkim <supakorn.kim@gmail.com>
2022-03-10 00:24:57 +0700

    update

--- contracts/JKCOIN.sol
+++ contracts/JKCOIN.sol
@@ -2,32 +2,14 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract JKCOIN is ERC20, ERC20Burnable, Pausable, AccessControl {
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-
+contract JKCOIN is ERC20 {
+    
     constructor() ERC20("JK COIN", "JK") {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
-        _mint(msg.sender, 1000000000 * 10 ** decimals());
-    }
-
-    function pause() public onlyRole(PAUSER_ROLE) {
-        _pause();
-    }
-
-    function unpause() public onlyRole(PAUSER_ROLE) {
-        _unpause();
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        whenNotPaused
-        override
-    {
-        super._beforeTokenTransfer(from, to, amount);
+        _mint(0xd98b049DC12Ac5825a4eD28A46095Eb8fBcE77BA, 300000000 * 10 ** decimals()); // reward
+        _mint(0xaed28707Bd26F6e7091736fd5768Ef3ceB4a6462, 150000000 * 10 ** decimals()); // eco system
+        _mint(0xC3FcAB357B9F2630Dd47cC5b800a41260b8F0590, 348000000 * 10 ** decimals()); // marketing
+        _mint(0x0fc4541C27c395784B648A28E2AD5E150920b79A, 200000000 * 10 ** decimals()); // team
+        _mint(0xecb9c8425A48afcB1547153D4C69Ddd104330C74, 2000000 * 10 ** decimals()); // private sale
     }
 }
